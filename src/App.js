c0302a3b83f8be5f2e0996edcbf74076d9b46fc0
egy351 <129901135+egy351@users.noreply.github.com>
2023-05-22 12:52:48 +0200

    getting ids from hnApi with axios & async function

--- src/App.js
+++ src/App.js
@@ -1,3 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getStoryIds } from './services/hackerNewsApi';
 
-export const App = () => <p>Hello</p>;
+export const App = () => {
+  const [storyIds, setStoryIds] = useState([]);
+
+  useEffect(() => {
+    setStoryIds('hell00o');
+  }, []);
+
+  return <p>{storyIds}</p>;
+};
