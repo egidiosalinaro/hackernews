98caf9ae2825068a57f624e7db709f4f7a907aaa
egy351 <129901135+egy351@users.noreply.github.com>
2023-05-23 12:23:30 +0200

    moved loadMoreButton function in hook folder

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
 import { StoriesContainer } from './containers/storiesContainer';
+import { loadMoreButton } from './hooks/loadMoreButton';
+
+loadMoreButton();
 
 export const App = () => <StoriesContainer />;
