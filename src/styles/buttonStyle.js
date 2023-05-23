f111dc33216fd3c9eb4006814ddf630966605323
egy351 <129901135+egy351@users.noreply.github.com>
2023-05-23 20:28:15 +0200

    test style for button during loading

--- src/styles/buttonStyle.js
+++ src/styles/buttonStyle.js
@@ -18,4 +18,92 @@ export const ButtonWrapper = styled.button`
     background-color: grey;
     color: inherit;
   }
+
+  .loading {
+    display: none;
+    height: 32px;
+    width: 32px;
+    margin: 0 auto;
+    animation: loader1 3s linear infinite;
+  }
+
+  @keyframes loader1 {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+
+  .loading span {
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    width: 32px;
+    height: 32px;
+    clip: rect(16px, 32px, 32px, 0);
+    animation: loader2 1.5s cubic-bezier(0.77, 0, 0.175, 1) infinite;
+  }
+
+  @keyframes loader2 {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+
+  .loading span::before {
+    content: '';
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    width: 32px;
+    height: 32px;
+    border: 3px solid transparent;
+    border-top: 3px solid white;
+    border-radius: 50%;
+    animation: loader3 1.5s cubic-bezier(0.77, 0, 0.175, 1) infinite;
+  }
+
+  @keyframes loader3 {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+
+  .loading span::after {
+    content: '';
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    width: 32px;
+    height: 32px;
+    border: 3px solid grey;
+    border-radius: 50%;
+  }
+
+  &.show-loader {
+    width: 52px;
+    background-color: white;
+    text-indent: -9999px;
+
+    & .loading {
+      display: block;
+    }
+  }
 `;
