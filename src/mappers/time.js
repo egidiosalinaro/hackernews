254382679d9e603d66da54b68122b4722a5a648e
egy351 <129901135+egy351@users.noreply.github.com>
2023-05-27 21:44:21 +0200

    time mapper updated

--- src/mappers/time.js
+++ src/mappers/time.js
@@ -3,27 +3,27 @@ export const mapTime = timestamp => {
 
   let interval = Math.floor(seconds / 31536000);
 
-  if (interval > 1) {
-    return `${interval} years`;
+  if (interval >= 1) {
+    return `${interval}y`;
   }
   interval = Math.floor(seconds / 2592000);
 
-  if (interval > 1) {
-    return `${interval} months`;
+  if (interval >= 1) {
+    return `${interval}months`;
   }
   interval = Math.floor(seconds / 86400);
 
-  if (interval > 1) {
-    return `${interval} days`;
+  if (interval >= 1) {
+    return `${interval}d`;
   }
   interval = Math.floor(seconds / 3600);
 
-  if (interval > 1) {
+  if (interval >= 1) {
     return `${interval}h`;
   }
   interval = Math.floor(seconds / 60);
 
-  if (interval > 1) {
+  if (interval >= 1) {
     return `${interval}m`;
   }
 
