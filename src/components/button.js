7653176883052a173b6596743b441832d095430b
egy351 <129901135+egy351@users.noreply.github.com>
2023-05-31 13:48:14 +0200

    tests

--- src/components/button.js
+++ src/components/button.js
@@ -3,7 +3,7 @@ import { ButtonWrapper } from '../styles/buttonStyle.js';
 import { loadMoreButton } from '../hooks/loadMoreButton.js';
 
 export const Button = () => (
-  <ButtonWrapper id="button" onLoadStart={loadMoreButton}>
+  <ButtonWrapper id="button" onFocus={loadMoreButton} onClick={loadMoreButton}>
     LOAD MORE
     <span className="loading">
       <span></span>
